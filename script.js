28a15e55dbcb9e2ef9923aba995844f1ba600ab4
Liza <elizabethspivak99@gmail.com>
2021-01-16 23:00:23 +0300

    master

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let todo = document.querySelector('ul');
-
-todo.addEventListener('click', function (el) {
-    el.classList.toggle('li')
-});
-
 function newElement() {
-    var li = document.createElement('li');
+    //создаем li
+    let li = document.createElement('li');
+    // получаем значение из input
     let inputValue = document.getElementById("example").value;
-    var t = document.createTextNode(inputValue);
+    console.log(inputValue);
+    // в текстовый узел t засовываем строку с данными из inputValue
+    let t = document.createTextNode(inputValue);
+    // засовываем текстовый узел t в конец списка li
     li.appendChild(t);
+    // кладем наше li со значением в конец нашего списка
     document.getElementById('todo').appendChild(li);
 
+    // чистим поле input после ввода значения
     document.getElementById('example').value = ''
 }
 
